f0d45c60a234852f3f6f6f3e234f0a44427c2f29
Mark McCarthy <mm3guides@gmail.com>
2024-10-28 14:58:16 +0000

    conditional rendering added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import Box from './components/box'
 import Post from './components/post';
 
 function App() {
 
-const [arrayOfPictures,setArrayOfPictures] = useState([])
-
+const [arrayOfPictures,setArrayOfPictures] = useState([]);
+const [isLoggedIn,setIsLoggedIn] = useState(false);
 
 async function fetchImages() {
   const fetchedImages = await fetch("https://picsum.photos/v2/list?page=3&limit=25");
@@ -15,6 +14,12 @@ async function fetchImages() {
   console.log("hello");
 }
 
+function loginLogout() {
+  setIsLoggedIn(!isLoggedIn);
+  console.log(isLoggedIn);
+}
+
+
 useEffect(() => {
   fetchImages() 
 },[])
@@ -22,15 +27,19 @@ useEffect(() => {
 
   return (
     <>
-    <button onClick={() => fetchImages()}>Get Images</button>
+    <button onClick={() => loginLogout()}>{isLoggedIn ? <p>Logout</p> : <p>Login</p>}</button>
+    <br></br>
     <h1>My React Refresh App</h1>
+  {isLoggedIn && 
+  <>
     {arrayOfPictures.map((item, index) => {
       return (
         // eslint-disable-next-line react/jsx-key
         <Post url={item.download_url} author={item.author}></Post>
       )
     })}
-
+    </>
+}
     </>
   )
 }
